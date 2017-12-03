2238a5d37775928ea999de0ff7762f04a7596703
Cherish Clark <cherishclark@Cherishs-MacBook-Pro.local>
2017-12-03 15:17:22 -0700

    second attempt

---
+++ config/routes.rb
@@ -0,0 +1,12 @@
+Rails.application.routes.draw do
+  get 'pages/welcome'
+
+  get 'pages/about'
+
+  get 'pages/contact'
+
+  get 'pages/shop'
+
+  resources :blogs
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
